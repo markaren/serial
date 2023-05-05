7e9f878f61e04cd652f620488e9d7534b77244b8
Lars Ivar Hatledal <laht@ntnu.no>
2023-05-05 14:04:18 +0200

    restructure project

--- examples/serial_example.cc
+++ examples/serial_example.cc
@@ -16,9 +16,9 @@
  * </pre>
  */
 
-#include <string>
-#include <iostream>
 #include <cstdio>
+#include <iostream>
+#include <string>
 
 // OS Specific sleep
 #ifdef _WIN32
@@ -29,59 +29,54 @@
 
 #include "serial/serial.h"
 
-using std::string;
-using std::exception;
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
+using std::exception;
+using std::string;
 using std::vector;
 
 void my_sleep(unsigned long milliseconds) {
 #ifdef _WIN32
-      Sleep(milliseconds); // 100 ms
+  Sleep(milliseconds); // 100 ms
 #else
-      usleep(milliseconds*1000); // 100 ms
+  usleep(milliseconds * 1000); // 100 ms
 #endif
 }
 
-void enumerate_ports()
-{
-	vector<serial::PortInfo> devices_found = serial::list_ports();
+void enumerate_ports() {
+  vector<serial::PortInfo> devices_found = serial::list_ports();
 
-	vector<serial::PortInfo>::iterator iter = devices_found.begin();
+  auto iter = devices_found.begin();
 
-	while( iter != devices_found.end() )
-	{
-		serial::PortInfo device = *iter++;
+  while (iter != devices_found.end()) {
+    serial::PortInfo device = *iter++;
 
-		printf( "(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(),
-     device.hardware_id.c_str() );
-	}
+    printf("(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(),
+           device.hardware_id.c_str());
+  }
 }
 
-void print_usage()
-{
-	cerr << "Usage: test_serial {-e|<serial port address>} ";
-    cerr << "<baudrate> [test string]" << endl;
+void print_usage() {
+  cerr << "Usage: test_serial {-e|<serial port address>} ";
+  cerr << "<baudrate> [test string]" << endl;
 }
 
-int run(int argc, char **argv)
-{
-  if(argc < 2) {
-	  print_usage();
+int run(int argc, char **argv) {
+  if (argc < 2) {
+    print_usage();
     return 0;
   }
 
   // Argument 1 is the serial port or enumerate flag
   string port(argv[1]);
 
-  if( port == "-e" ) {
-	  enumerate_ports();
-	  return 0;
-  }
-  else if( argc < 3 ) {
-	  print_usage();
-	  return 1;
+  if (port == "-e") {
+    enumerate_ports();
+    return 0;
+  } else if (argc < 3) {
+    print_usage();
+    return 1;
   }
 
   // Argument 2 is the baudrate
@@ -96,7 +91,7 @@ int run(int argc, char **argv)
   serial::Serial my_serial(port, baud, serial::Timeout::simpleTimeout(1000));
 
   cout << "Is the serial port open?";
-  if(my_serial.isOpen())
+  if (my_serial.isOpen())
     cout << " Yes." << endl;
   else
     cout << " No." << endl;
@@ -115,7 +110,7 @@ int run(int argc, char **argv)
   while (count < 10) {
     size_t bytes_wrote = my_serial.write(test_string);
 
-    string result = my_serial.read(test_string.length()+1);
+    string result = my_serial.read(test_string.length() + 1);
 
     cout << "Iteration: " << count << ", Bytes written: ";
     cout << bytes_wrote << ", Bytes read: ";
@@ -131,7 +126,7 @@ int run(int argc, char **argv)
   while (count < 10) {
     size_t bytes_wrote = my_serial.write(test_string);
 
-    string result = my_serial.read(test_string.length()+1);
+    string result = my_serial.read(test_string.length() + 1);
 
     cout << "Iteration: " << count << ", Bytes written: ";
     cout << bytes_wrote << ", Bytes read: ";
@@ -161,7 +156,7 @@ int run(int argc, char **argv)
   while (count < 10) {
     size_t bytes_wrote = my_serial.write(test_string);
 
-    string result = my_serial.read(test_string.length()-1);
+    string result = my_serial.read(test_string.length() - 1);
 
     cout << "Iteration: " << count << ", Bytes written: ";
     cout << bytes_wrote << ", Bytes read: ";
